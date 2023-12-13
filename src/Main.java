a1c1f8e5fbd6e04e139c54021f2510701951e7f9
Ivan <ivanpiccioni.work@gmail.com>
2023-12-13 17:02:41 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        int numero = 8;
+        int numeroMassimo = 5;
+        int numeroMinimo = 1;
         try {
-            verificaRange(30, 1, 20);
-            System.out.println("Il numero è nel range specificato.");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            if (verificaRange(numero, numeroMassimo, numeroMinimo)) {
+                System.out.println("il numero è nel range");
+            }else{
+                System.out.println("il numero non è nel range");
+                }
+        } catch (ArithmeticException e) {
+            System.out.println("Errore non è nel range");
         }
-    }
 
-    public static void verificaRange(int numero, int minimo, int massimo) throws Exception {
-        if (numero < minimo || numero > massimo || minimo > massimo) {
-            throw new Exception("Il numero non è nel range specificato ");
-        }
+    }
+    public static boolean verificaRange(int numero,int numeroMassimo,int numeroMinimo){
+        return numero < numeroMassimo && numero > numeroMinimo;
     }
 }
-
